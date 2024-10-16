39f05876f9773e7722efc403347723e51e563933
sharksonk <sharksonk@gmail.com>
2024-10-16 18:05:13 +0300

    popravile

--- matrix.cpp
+++ matrix.cpp
@@ -1,15 +1,15 @@
 #include "matrix.hpp"
-void in(int** t, size_t m, size_t n){
-	for(size_t i = 0; i < m, ++i){
-		for(size_t j = 0; j < n; ++j){
+void in(int ** t, size_t M, size_t N){
+	for(size_t i = 0; i < M, ++i){
+		for(size_t j = 0; j < N; ++j){
 			std::cin >> t[i][j];
 		}
 	}
 }
-void out(const int * const * t, size_t m, size_t n){
-	for(size_t i = 0; i <  m, ++i){
+void out(const int * const * t, size_t M, size_t N){
+	for(size_t i = 0; i <  M, ++i){
 		std::cout << t[i][0];
-		for(size_t j = 1; j < n, ++j){
+		for(size_t j = 1; j < N, ++j){
 			std::cout << " " << t [i][j]){
 		}
 		std::cout << "\n";
