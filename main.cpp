0cefa1fae4c9de3a0ecd190e44d050a1033b8e7b
sharksonk <sharksonk@gmail.com>
2024-10-19 22:05:30 +0300

    izmenenie otstupa

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 int** create(size_t m, size_t n);
 void destroy(size_t m, size_t n);
 int main() {
-	size_t m = 0, n = 0;
-	std::cin >>  m >> n;
-	int **t = create (m, n);
+  size_t m = 0, n = 0;
+  std::cin >>  m >> n;
+  int **t = create (m, n);
 }
