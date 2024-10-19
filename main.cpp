6544272f0ce1686983f894ab7e1fc0ba24ab6ba5
sharksonk <sharksonk@gmail.com>
2024-10-19 21:41:22 +0300

    from big to little

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-int** create(size_t M, size_t N);
-void destroy(size_t M, size_t N);
+int** create(size_t m, size_t n);
+void destroy(size_t m, size_t n);
 int main() {
-	size_t M = 0, N = 0;
-	std::cin >>  M >> N;
-	int **t = create (M, N);
+	size_t m = 0, n = 0;
+	std::cin >>  m >> n;
+	int **t = create (m, n);
 }
