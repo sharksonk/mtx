cd22b1b08ac341746dbfc88a75832abb64248827
sharksonk <sharksonk@gmail.com>
2024-10-19 22:57:56 +0300

    xz kak nazvat

--- matrix.hpp
+++ matrix.hpp
@@ -1,5 +1,7 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
-void destroy(int** t, size_t M, size_t N);
-int** create(size_t M, size_t N);
+void destroy(int** t, size_t m, size_t n);
+int** create(size_t m, size_t n);
+void inm(int ** t, size_t m, size_t n);
+void outm(const int * const * t, size_t m, size_t n);
 #endif
