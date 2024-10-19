7f52487dc06f93cb763fe162098ee543cd176a6a
sharksonk <sharksonk@gmail.com>
2024-10-19 23:04:08 +0300

    dopisala

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-int** create(size_t m, size_t n);
-void destroy(size_t m, size_t n);
+#include "matrix.hpp"
 int main() {
   size_t m = 0, n = 0;
   std::cin >>  m >> n;
-  int **t = create (m, n);
+  if (!std::cin) {
+    std::cerr << "Not a number\n"
+    return 1;
+  }
+  int** t  = nullptr;
+  try {
+    t = creatematrix(m,n);
+  } catch (const std::bad_alloc & e) {
+    std::cerr << "Out of memory\n";
+    return 1;
+  }
+  inm(t, m, n);
+  if (!std::cin) {
+    std::cerr << "Not a number\n";
+    return 1;
+  }
+  outm(t, m, n);
+  destroy(t, m);
 }
