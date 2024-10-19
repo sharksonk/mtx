8f800d5a59338275a9160e7af468e988abdcaec8
sharksonk <sharksonk@gmail.com>
2024-10-19 22:03:58 +0300

    from big to little

--- matrix.cpp
+++ matrix.cpp
@@ -1,17 +1,17 @@
 #include "matrix.hpp"
-void in(int ** t, size_t M, size_t N){
-	for(size_t i = 0; i < M, ++i){
-		for(size_t j = 0; j < N; ++j){
-			std::cin >> t[i][j];
-		}
-	}
+void in(int ** t, size_t m, size_t n){
+  for(size_t i = 0; i < m, ++i){
+    for(size_t j = 0; j < n; ++j){
+      std::cin >> t[i][j];
+    }
+  }
 }
-void out(const int * const * t, size_t M, size_t N){
-	for(size_t i = 0; i <  M, ++i){
-		std::cout << t[i][0];
-		for(size_t j = 1; j < N, ++j){
-			std::cout << " " << t [i][j]){
-		}
-		std::cout << "\n";
-	}
+void out(const int * const * t, size_t m, size_t n){
+  for(size_t i = 0; i <  m, ++i){
+    std::cout << t[i][0];
+    for(size_t j = 1; j < n, ++j){
+      std::cout << " " << t [i][j]){
+    }
+    std::cout << "\n";
+  }
 }
